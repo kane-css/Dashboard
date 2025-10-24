b7f3e9fd2b0fa4edd98cc20af15978d573728630
kane-css <deverokane@gmail.com>
2025-10-24 15:38:52 +0800

    oji gatdula

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dashboard/', // needed for GitHub Pages
-  build: {
-    outDir: 'dist',
-  },
-})
+  base: '/Dashboard/', // 👈 must match repo name exactly
+});
