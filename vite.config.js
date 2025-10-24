02deec2e9ba38bbcd0d04f8a6f02e98cff853556
kane-css <deverokane@gmail.com>
2025-10-25 00:26:29 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dashboard/', // 👈 your repo name, with slashes!
+  base: '/Dashboard/', // ✅ must match your repo name
   build: {
     outDir: 'dist',
   },
