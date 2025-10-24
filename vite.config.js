f07afbe523a31c8bf0ecf8ea0b3fb007cac2057b
kane-css <deverokane@gmail.com>
2025-10-24 23:16:25 +0800

    build: deploy latest version

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Dashboard/', // 👈 must match repo name exactly
+  base: '/Dashboard/', // ⚠️ must match repo name exactly (case-sensitive)
 });
